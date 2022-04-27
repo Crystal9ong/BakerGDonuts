80f8b3fb895b607852b6cabf5196706ba3608947
crys9ong <cyblackwing@hotmail.com>
2022-04-27 23:43:54 +0800

    faqs done

---
+++ js/faqs.js
@@ -0,0 +1,7 @@
+var change = document.querySelector(".faqs_title");
+var afterStyle = window.getComputedStyle(change, "::after");
+console.log(change);
+
+// questoinBox.addEventListener("click",function(){
+
+// })
